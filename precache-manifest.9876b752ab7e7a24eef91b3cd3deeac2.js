d2df603a4a6fc68024ee9a8fb30d6eeeeeb84435
kunjani <chris.sadler@gmail.com>
2019-05-03 14:39:48 +0100

    Updates

---
+++ precache-manifest.9876b752ab7e7a24eef91b3cd3deeac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bfde0067da3d5f3589457f42f66022",
+    "url": "/PageTest/index.html"
+  },
+  {
+    "revision": "ae9b923135e6df5d49a8",
+    "url": "/PageTest/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/PageTest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ae9b923135e6df5d49a8",
+    "url": "/PageTest/static/js/main.aa2363c3.chunk.js"
+  },
+  {
+    "revision": "f1ff7590806d29e08e1c",
+    "url": "/PageTest/static/js/runtime~main.8ddfcd54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PageTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
