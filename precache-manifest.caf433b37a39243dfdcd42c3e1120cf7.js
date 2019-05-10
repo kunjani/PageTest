bcf7033e394813181499d3f1080ac7091728570e
kunjani <chris.sadler@gmail.com>
2019-05-10 17:49:37 +0100

    Updates

--- precache-manifest.caf433b37a39243dfdcd42c3e1120cf7.js
+++ precache-manifest.caf433b37a39243dfdcd42c3e1120cf7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8923db7d05b5bb9caabcac51237e72c6",
+    "revision": "d88911929531af78ff98e17485bb43f2",
     "url": "/PageTest/index.html"
   },
   {
-    "revision": "9f3962094ccdcded93e9",
-    "url": "/PageTest/static/css/main.ec69dbad.chunk.css"
+    "revision": "afbeff59a977064040dd",
+    "url": "/PageTest/static/css/main.92948b96.chunk.css"
   },
   {
     "revision": "b121525a03c81e9a8dc4",
     "url": "/PageTest/static/js/2.747baf39.chunk.js"
   },
   {
-    "revision": "9f3962094ccdcded93e9",
-    "url": "/PageTest/static/js/main.f5c9cbfb.chunk.js"
+    "revision": "afbeff59a977064040dd",
+    "url": "/PageTest/static/js/main.58c6dcca.chunk.js"
   },
   {
     "revision": "f1ff7590806d29e08e1c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PageTest/precache-manifest.8504b7965496c555f54fe60857d2db84.js"
+  "/PageTest/precache-manifest.caf433b37a39243dfdcd42c3e1120cf7.js"
 );
 
 self.addEventListener('message', (event) => {
